fb7ae9983621d76589a988506f3e2fb5681420ac
DaMichel <mw.pub@welter-4d.de>
2016-10-16 22:13:31 +0200

    * included updated toolbar header by Blizzy * removed reference to KSPUtil.dll

--- ToolbarWrapper.cs
+++ ToolbarWrapper.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2013-2014, Maik Schreiber
+Copyright (c) 2013-2016, Maik Schreiber
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -25,13 +25,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
 
 // TODO: Change to your plugin's namespace here.
-namespace DaMichelToolbarSuperWrapper {
+namespace DaMichelToolbarSuperWrapper
+{
 
 
 
@@ -402,7 +402,7 @@ namespace DaMichelToolbarSuperWrapper {
 	/// <example>
 	/// <code>
 	/// IButton button = ...
-	/// button.Visibility = new GameScenesVisibility(GameScenes.EDITOR, GameScenes.SPH);
+	/// button.Visibility = new GameScenesVisibility(GameScenes.EDITOR, GameScenes.FLIGHT);
 	/// </code>
 	/// </example>
 	/// <seealso cref="IButton.Visibility"/>
@@ -726,9 +726,13 @@ namespace DaMichelToolbarSuperWrapper {
 		}
 
 		internal static Type getType(string name) {
-			return AssemblyLoader.loadedAssemblies
-				.SelectMany(a => a.assembly.GetExportedTypes())
-				.SingleOrDefault(t => t.FullName == name);
+			Type type = null;
+			AssemblyLoader.loadedAssemblies.TypeOperation(t => {
+				if (t.FullName == name) {
+					type = t;
+				}
+			});
+			return type;
 		}
 
 		internal static PropertyInfo getProperty(Type type, string name) {
